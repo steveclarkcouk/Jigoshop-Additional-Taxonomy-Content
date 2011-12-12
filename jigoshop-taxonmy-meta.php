13b5eaafc90bd2c322892efe75de26836906cf0e
Steve Clark <info@clark-studios.co.uk>
2011-12-12 10:45:31 +0000

    fix #5
    Checked it object and returned slug on taxonomy pages.

--- jigoshop-taxonmy-meta.php
+++ jigoshop-taxonmy-meta.php
@@ -18,6 +18,7 @@ $jct_admin->setData('prefix', 'jigo_tax_');
 function jigoshop_additonal_taxonomy()
 {
 	global $term, $jct_admin;
+	if(is_object($term)) $term = $term->slug;
 	$term_additonal_content = get_option($jct_admin->data['prefix'] . 'tax_' . $term);
 	if($term_additonal_content) echo '<div class="more-tax-desc">' . html_entity_decode(wp_richedit_pre( stripslashes ($term_additonal_content) )) . '</div>';
 }
